7d007cdf1bfa13b500eceffb0fd618f855a2e431
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2019-07-28 17:52:45 -0400

    created animated SVG properties

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from "react";
-import { View, SafeAreaView, Dimensions } from "react-native";
-import { Svg } from "expo";
+import { SafeAreaView, Dimensions, StyleSheet, Animated } from "react-native";
+import * as Svg from "react-native-svg";
 import * as shape from "d3-shape";
 
 interface TabBarProps {}
@@ -12,15 +12,36 @@ const tabs = [
   { name: "box" },
   { name: "user" }
 ];
+
+const { width } = Dimensions.get("window");
+
 const tabWidth = width / tabs.length;
-const width = Dimensions.get("window");
 const height = 64;
 const { Path } = Svg;
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
 
 const left = shape
   .line()
   .x((d) => d.x)
   .y((d) => d.y)([{ x: 0, y: 0 }, { x: width, y: 0 }]);
+
+const tab = shape
+  .line()
+  .x((d) => d.x)
+  .y((d) => d.y)
+  .curve(shape.curveBasis)([
+  {
+    x: width,
+    y: 0
+  },
+  { x: width + 5, y: 0 },
+  { x: width + 10, y: 10 },
+  { x: width + 15, y: height },
+  { x: width + tabWidth - 15, y: height },
+  { x: width + tabWidth - 10, y: 10 },
+  { x: width + tabWidth - 5, y: 0 },
+  { x: width + tabWidth, y: 0 }
+]);
 
 const right = shape
   .line()
@@ -34,11 +55,24 @@ const right = shape
 
 const d = `${left} ${tab} ${right}`;
 
-class TabBar extends Component<TabBarProps> {
+class TabBar extends PureComponent<TabBarProps> {
   state = {};
   render() {
-    return <View>{}</View>;
+    return (
+      <>
+        <AnimatedSvg width={width * 2} {...{ height }}>
+          <Path {...{ d }} fill="white" />
+        </AnimatedSvg>
+        <SafeAreaView style={styles.safeArea} />
+      </>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: "white"
+  }
+});
 
 export default TabBar;
